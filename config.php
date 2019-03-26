e5e2de068d7f38e32413a51d203ebb1ac50b13de
polodev <polodev10@gmail.com>
2019-03-27 03:02:48 +0600

    updateupdate"

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 require(__dir__ . '/Helper.php');
 return [
-    // 'baseUrl' => 'https://polodev.github.io/jigsaw-blog',
+    'baseUrl' => 'https://polodev.github.io/jigsaw-blog',
     'baseUrl' => '',
     'production' => false,
     'siteName' => 'Blog Starter Template',
     'siteDescription' => 'Generate an elegant blog with Jigsaw',
     'siteAuthor' => 'Author Name',
     'build' => [
-        // 'destination' => 'docs',
-        'destination' => 'build_local',
+        'destination' => 'docs',
+        // 'destination' => 'build_local',
     ],
 
     // collections
